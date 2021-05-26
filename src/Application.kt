ad6c8a5750b1798ccb244d2fde3f6372a89e4a65
Alexander Jaroch <alexander-jaroch@live.com>
2021-05-26 23:02:53 +0200

    project reset for cloning as base

--- src/Application.kt
+++ src/Application.kt
@@ -1,25 +1,21 @@
 package com.todoapi
 
 import com.todoapi.files.staticRoutes
-import com.todoapi.files.toDoRoutes
-import com.todoapi.todo.ToDoList
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
 import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
-
-val toDoList: ToDoList = ToDoList()
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-    toDoList.add("Kotlin vorstellen")
-
     routing {
-        toDoRoutes()
+        get("/") {
+            call.respondText("Hello World!", ContentType.Text.Plain);
+        }
+
         staticRoutes()
     }
 }
--- src/todo/ToDoItem.kt
+++
@@ -1,9 +0,0 @@
-package com.todoapi.todo
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class ToDoItemData(val text: String)
-
-@Serializable
-data class ToDoItem(val id: Long, var checked: Boolean, var text: String)
